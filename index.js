2ab0b04df666b0293fd12db2bedfafbe0c18dde8
Houser97 <arturo.riverar97@gmail.com>
2022-08-12 19:31:12 -0500

    Add functions to sanitize array to be converted to BST

--- index.js
+++ index.js
@@ -27,3 +27,68 @@ function sortedArrayToBST(array, start, end){
 
     return node;
 }
+
+const mergeSort = (array) => {
+    if(array.length <= 1) return array;
+    let result = [];
+
+    let firstHalf = array.slice(0, Math.ceil(array.length/2));
+    let secondHalf = array.slice(Math.ceil(array.length/2));
+
+    if(firstHalf.length >= 2){
+        firstHalf = mergeSort(firstHalf);
+    };
+
+    if(secondHalf.length >= 2){
+        secondHalf = mergeSort(secondHalf);
+    };
+
+    if(firstHalf.length === 1 && secondHalf.length === 1){
+        if(firstHalf[0] <= secondHalf[0]){
+            return [...firstHalf, ...secondHalf];
+        } else {
+            return [...secondHalf, ...firstHalf];
+        }
+    };
+
+    let continueLoop = true;
+    while(continueLoop){
+        if(firstHalf[0] < secondHalf[0]){
+            result.push(firstHalf.shift());
+        } else if(firstHalf[0] > secondHalf[0]){
+            result.push(secondHalf.shift());
+        } else if(firstHalf[0] === secondHalf[0]){
+            result.push(firstHalf.shift());
+            result.push(secondHalf.shift());
+        }
+        if(firstHalf.length === 0 && secondHalf.length>0){
+            result = [...result, ...secondHalf];
+            continueLoop = false;
+        } else if(firstHalf.length > 0 && secondHalf.length === 0){
+            result = [...result, ...firstHalf];
+            continueLoop = false;
+        } else if(firstHalf.length === 0 && secondHalf.length === 0){
+            continueLoop = false;
+        }
+    };
+
+    return result;
+ };
+
+const removeDuplicates = (array) => {
+    let arrayResult = [];
+
+    array.forEach(number => {
+        if(!arrayResult.includes(number)){
+            arrayResult.push(number);
+        };
+    });
+    return arrayResult;
+};
+
+const sanitizeArray = (array) => {
+    let result;
+    result = removeDuplicates(array);
+    result = mergeSort(result);
+    return result;
+}
